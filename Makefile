b3eb219baa4d00fc7c38c8674846d9b335cb0e2d
Richard Walker <rwalker@redhat.com>
2020-03-30 12:23:56 +0100

    added 3musketeer files

---
+++ Makefile
@@ -0,0 +1,4 @@
+COMPOSE_SERVICE='alpine-custom'
+
+demo-make-docker-compose:
+    docker-compose run --rm $(COMPOSE_SERVICE) ./scripts/demo.sh
---
+++ scripts/demo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Demo using the 3 Musketeers pattern" > /opt/demo/3musketeer-demo.txt
+echo "The value of foo = ${foo}" >> /opt/demo/3musketeer-demo.txt
+
